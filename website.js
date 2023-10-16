3f4bdcd97cc101c336a5593d49a78153896ad93d
Thieen226 <t.ho1@my.metroed.net>
2023-10-16 09:37:45 -0700

    add

--- website.js
+++ website.js
@@ -1,6 +1,10 @@
 const submitButton = document.getElementById("submit");
 
 function submitSucess(){
-    window.location.reload();
+    const changeDisplay= document.getElementById("after_submit");
+    changeDisplay.style.alert="none";
+    changeDisplay.textContent="Thank you! You have sucessfully submitted a report form, we will contact you soon!";
+    changeDisplay.style.margin="5rem";
+    changeDisplay.style.color="#4CAF50";
 }
 submitButton.addEventListener("click", submitSucess);
